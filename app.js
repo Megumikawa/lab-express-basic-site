87c30878cda53f1da803debe680c09ffb4cd0cdc
Megumikawa <megumi19901016@gmail.com>
2021-02-02 20:23:50 +0100

    create home and about

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+app.use( express.static(__dirname + '/public'))
+
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname + '/views/home.html')
+})
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html')
+})
+app.get('/works', (req, res) => {
+  res.sendFile(__dirname + '/views/works.html')
+})
+app.get('/album', (req,res) => {
+  res.sendFile(__dirname + '/views/album.html')
+})
+app.get('*', (req, res) => {
+  res.send(__dirname + 'views/404.html')
+})
+app.listen(3000)
